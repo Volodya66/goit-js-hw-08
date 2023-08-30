88cd2bd2d961afbcffa50cbf36aaa7a282bfaef1
Volodya66 <130245015+Volodya66@users.noreply.github.com>
2023-08-30 22:10:18 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,52 +1,36 @@
 
 import throttle from 'lodash.throttle';
 
+let userData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+
 const form = document.querySelector('.feedback-form');
-
-
 const userEmail = form.querySelector('input[type="email"]');
 const userMessage = form.querySelector('textarea[name="message"]');
-const availabilityOfData = localStorage.getItem('feedback-form-state');
-console.log(availabilityOfData);
-console.log(userEmail);
 
-// ! присутність даних в локальному сховищі 
 
-if (availabilityOfData !== null) {
-    const data = JSON.parse(availabilityOfData);
-    const { email, message } = data;
-    userEmail.value = email;
-    userMessage.value = message;
-}
 
-console.log(availabilityOfData);
+userEmail.value = userData.email || '';
+userMessage.value = userData.message || '';
+
 
 // ? відправка даних на локалку з затримкою в 500мс
-form.addEventListener("input", throttle(function () {
-    const data = {
+form.addEventListener('input', throttle(function () {
+    userData = {
         email: userEmail.value,
         message: userMessage.value
     };
-    localStorage.setItem("feedback-form-state",JSON.stringify(data))
-},500));
-
-
-
-
-
+    localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+}, 500));
 
 // ? відміна оновлення сторінки і вивід даних інпуту в консоль
-form.addEventListener("submit", noSubForm)
+form.addEventListener('submit', noSubForm)
 function noSubForm(evt) {
     evt.preventDefault();
 
-    const formData = {
-            email: userMessage.value,
-            message: userMessage.value,
-        };
-        console.log(formData);
+    console.log(userData);
     localStorage.removeItem('feedback-form-state');
-    userEmail.value = "";
-    userMessage.value = "";
-    
+    userEmail.value = '';
+    userMessage.value = '';
+    userData = {};
 };
+
